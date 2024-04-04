1f22b5bd97e46135ce35ec59e92efe7bf480414b
avitej-iyer <iyeravitej2@gmail.com>
2024-04-04 16:00:30 -0400

    minor changes

--- Project 3/main.m
+++ Project 3/main.m
@@ -9,20 +9,23 @@ grayscale_ants = convertToGrayScale(folderPath);
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Step 2
 
-simple_background_sub_output = simpleBackgroundSubtraction(grayscale_ants);
-simple_frame_diff_output = simpleFrameDifferencing(grayscale_ants);
+lambda = 90;
+new_simple_back_sub_out = new_simpleBackgroundDifferencing(grayscale_ants, lambda);
 
-alpha = 0.5; % halways between simple frame diff and bg subtraction
+lambda = 40;
+new_simple_frame_diff_out = new_simpleFrameDifferencing(grayscale_ants, lambda);
 
-adaptive_bg_diff_output = adaptiveBackgroundSubtraction(grayscale_ants, alpha);
+lambda = 40;
+alpha = 0.1;
+new_adaptive_background_sub_out = new_adaptiveBackgroundSubtraction(grayscale_ants, alpha, lambda);
+
+lambda = 40;
+gamma = 50;
+new_persistent_frame_diff_out = new_persistentFrameDifferencing(grayscale_ants, lambda, gamma);
 
 
-thresholdValue = 25; 
-g = 50;  % Decay constant
-persistent_frame_differencing_output = persistentFrameDifferencing(grayscale_ants, thresholdValue, g);
+quad_panel_ants = createQuadPanelImages(new_simple_back_sub_out, new_simple_frame_diff_out, new_adaptive_background_sub_out, new_persistent_frame_diff_out);
 
-quad_panel_ants = createQuadPanelImages(simple_background_sub_output, simple_frame_diff_output, adaptive_bg_diff_output, persistent_frame_differencing_output);
+%saveOutputFrames(new_persistent_frame_diff_out, 'ants_output_quad');
 
-saveOutputFrames(quad_panel_ants, 'ants_output_quad');
-
-framesToVideo('ants_output_quad\', 15, 'ants_video');
+createVideoFromImageArray(quad_panel_ants, 15, 'video_outputs\', 'new_ants_vid_back_sub');
